889effd8ea1fa8190ab9b50076e33ecb83389c6b
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2026-07-11 12:06:26 -0300

    :star: feat: documentação para as DTOs

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { SignUpDto } from "../../auth/dto/signup.dto.ts";
+import { SignUpDto } from "../../auth/dto/signup.dto";
 import { IsBoolean, IsOptional } from "@nestjs/class-validator";
+import {ApiPropertyOptional} from "@nestjs/swagger";
 
 export class UpdateUserDto extends PartialType(SignUpDto) {
+    @ApiPropertyOptional({
+        description: "Definição se o usuário é administrador",
+        example: false
+    })
     @IsOptional()
     @IsBoolean()
     isAdmin!: boolean;
